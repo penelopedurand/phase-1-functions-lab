4ee20901e6014ebf3187a2e8a2ec9d9a699217fc
Penelope Durand <penelope.durand@mail.goucher.edu>
2022-04-05 16:18:55 -0400

    commit tree 1499616f59d58e29741cf69efd859a47fe138c4d

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    const headq = 42;
+    if (someValue > 42) {
+        return someValue - 42
+    } else {
+        return 42 - someValue
+    }
+}
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264
+}
+
+function distanceFromHqInFeet(start, destination) {
+    return (Math.abs (destination - start) * 264)
+}
+function calculatesFarePrice(start, destination) {
+
+}
